441e517b8a6b68187ecb06176c98d6c2688f0e4a
Guilherme Henrique <44971353+MrGuizao@users.noreply.github.com>
2023-07-21 09:41:58 -0300

    Exercício FILTRAGEM DE ITEM 21/07

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,43 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
   const [datas, setDatas] = useState([]);
-  const [click, setClick] = useState(null);
+  const [click, setClick] = useState('');
 
   const fetchData = () => {
     try {
-      fetch('https://jsonplaceholder.typicode.com/users')
+      fetch('http://files.cod3r.com.br/curso-js/funcionarios.json')
         .then(res => res.json())
         .then(data => setDatas(data))
     } catch (error) {
       console.log(error);
     }
   }
-  const handleClick = id => setClick(id);
+
+  const handleChange = (e) => setClick(e.target.value);
 
   useEffect(() => {
     fetchData();
   }, [])
 
+  const filteredData = datas.filter(data => data.nome.toLowerCase().includes(click.toLowerCase()))
+
   return (
     <>
-      {
-        datas.map(data =>
-          <div key={data.id} onClick={() => handleClick(data.id)}>
-            <h1 style={click === data.id ? { color: 'red' } : null}>{data.name}</h1>
-            <h3>{data.username}</h3>
-            <h3>{data.email}</h3>
-          </div>
+      <input type="text" placeholder='search...' value={click} onChange={handleChange} />
+      {filteredData.length === 0 ? (
+        <h1>Sem resultados</h1>
+      ) : (
+        filteredData.map(data =>
+          <main key={data.id}>
+            <h1>Nome: {data.nome} {data.sobrenome}</h1>
+            <h3>Gênero: {data.genero}</h3>
+            <h3>Salário: {data.salario}</h3>
+          </main>
         )
-      }
+      )}
     </>
   )
 }
 
-export default App
+export default App;
