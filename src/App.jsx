62d02a192f4fbc5ededc184711630978e64991ea
Guilherme Henrique <44971353+MrGuizao@users.noreply.github.com>
2023-07-28 10:16:14 -0300

    ex 28/07 FILTRO DE PESQUISA

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,29 @@
 import React, { useState } from 'react';
 
+const listaCompleta = [
+  { id: 1, nome: 'Item 1' },
+  { id: 2, nome: 'Item 2' },
+  { id: 3, nome: 'Item 3' },
+  { id: 4, nome: 'Item 4' },
+  { id: 5, nome: 'Item 5' },
+];
 const ListaFiltrada = () => {
-  const listaCompleta = [
-    { id: 1, nome: 'Item 1' },
-    { id: 2, nome: 'Item 2' },
-    { id: 3, nome: 'Item 3' },
-    { id: 4, nome: 'Item 4' },
-    { id: 5, nome: 'Item 5' },
-  ];
+  const [todos, setTodos] = useState(listaCompleta);
+  const [search, setSearch] = useState('');
 
-  const [filtro, setFiltro] = useState('todos');
-  const [listaFiltrada, setListaFiltrada] = useState(listaCompleta);
+  const handleChange = e => setSearch(e.target.value);
+  const filter = todos.filter(todo => todo.nome.toLowerCase().includes(search.toLowerCase()))
 
-  const handleFiltroChange = (e) => {
-    const filtroSelecionado = e.target.value;
-    setFiltro(filtroSelecionado);
-
-    if (filtroSelecionado === 'todos') {
-      setListaFiltrada(listaCompleta);
-    } else {
-      const novaListaFiltrada = listaCompleta.filter((item) => item.id === parseInt(filtroSelecionado));
-      setListaFiltrada(novaListaFiltrada);
-    }
-  };
 
   return (
-    <div>
-      <h2>Lista Filtrada</h2>
-      <select value={filtro} onChange={handleFiltroChange}>
-        <option value="todos">Todos</option>
-        <option value="1">Item 1</option>
-        <option value="2">Item 2</option>
-        <option value="3">Item 3</option>
-        <option value="4">Item 4</option>
-        <option value="5">Item 5</option>
-      </select>
-      <ul>
-        {listaFiltrada.map((item) => (
-          <li key={item.id}>{item.nome}</li>
-        ))}
-      </ul>
-    </div>
+    <main>
+      <input type="text" placeholder='pesquisar...' value={search} onChange={handleChange} />
+      {
+        filter.length === 0 ? <h1>Sem resultado</h1>
+          :
+          filter.map(todo => <li key={todo.id}>{todo.nome}</li>)
+      }
+    </main>
   );
 };
 
