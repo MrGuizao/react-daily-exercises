c7a57da009ad0872a63dc8cd029cf84fc4066922
Guilherme Henrique <44971353+MrGuizao@users.noreply.github.com>
2023-07-31 10:44:51 -0300

    31/07 DELETE E MULTIPLE INPUTS

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,41 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react';
+import { useState } from 'react';
 
 const App = () => {
-  const [datas, setDatas] = useState([]);
-  const [value, setValue] = useState('');
-  const filtered = datas.filter(data => data.nome.toLowerCase().includes(value.toLowerCase()))
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState({ name: '', age: '', size: '' });
+  const [click, setClick] = useState(null);
 
-  const handleChange = e => setValue(e.target.value);
-
-  useEffect(() => {
-    try {
-      fetch('http://files.cod3r.com.br/curso-js/funcionarios.json')
-        .then(response => response.json())
-        .then(data => setDatas(data))
-    }
-    catch (error) {
-      console.log(error);
-    }
-  }, [])
+  const handleSubmit = e => {
+    e.preventDefault();
+    setTodos([...todos, task]);
+    setTask({ name: '', age: '', size: '' });
+  }
+  const handleChange = e => setTask({ ...task, id: crypto.randomUUID(), [e.target.name]: e.target.value })
+  const handleClick = id => setClick(id);
+  const handleDelete = id => setTodos(todos.filter(todo => todo.id !== id));
 
   return (
-    <main>
-      <input type="text" value={value} onChange={handleChange} />
-      {
-        filtered.length === 0 ?
-          <h1>sem resultado</h1>
-          :
-          filtered.map(data => <li key={data.id}>{data.nome}</li>)
-      }
-    </main>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={task.name} name='name' placeholder='name' onChange={handleChange} />
+        <input type="text" value={task.age} name='age' placeholder='age' onChange={handleChange} />
+        <input type="text" value={task.size} name='size' placeholder='size' onChange={handleChange} />
+        <button>add</button>
+      </form>
+      <ul>
+        {
+          todos.map(todo => <li
+            key={todo.id}
+            onClick={() => handleClick(todo.id)}
+            style={click === todo.id ? { textDecoration: 'line-through' } : null}>
+            {todo.name} tem {todo.age} anos de idade e altura de {todo.size} Cm
+            <span onClick={() => handleDelete(todo.id)}>❌</span>
+          </li>)
+        }
+      </ul>
+    </>
   )
 }
 
-export default App
+export default App;
