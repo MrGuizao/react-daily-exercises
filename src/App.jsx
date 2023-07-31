d2a0bdfe7d79aad60edb33280160325ed001975a
Guilherme Henrique <44971353+MrGuizao@users.noreply.github.com>
2023-07-31 09:50:26 -0300

    31/07 SEARCH FILTER

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react'
 
-const listaCompleta = [
-  { id: 1, nome: 'Item 1' },
-  { id: 2, nome: 'Item 2' },
-  { id: 3, nome: 'Item 3' },
-  { id: 4, nome: 'Item 4' },
-  { id: 5, nome: 'Item 5' },
-];
-const ListaFiltrada = () => {
-  const [todos, setTodos] = useState(listaCompleta);
-  const [search, setSearch] = useState('');
+const App = () => {
+  const [datas, setDatas] = useState([]);
+  const [value, setValue] = useState('');
+  const filtered = datas.filter(data => data.nome.toLowerCase().includes(value.toLowerCase()))
 
-  const handleChange = e => setSearch(e.target.value);
-  const filter = todos.filter(todo => todo.nome.toLowerCase().includes(search.toLowerCase()))
+  const handleChange = e => setValue(e.target.value);
 
+  useEffect(() => {
+    try {
+      fetch('http://files.cod3r.com.br/curso-js/funcionarios.json')
+        .then(response => response.json())
+        .then(data => setDatas(data))
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }, [])
 
   return (
     <main>
-      <input type="text" placeholder='pesquisar...' value={search} onChange={handleChange} />
+      <input type="text" value={value} onChange={handleChange} />
       {
-        filter.length === 0 ? <h1>Sem resultado</h1>
+        filtered.length === 0 ?
+          <h1>sem resultado</h1>
           :
-          filter.map(todo => <li key={todo.id}>{todo.nome}</li>)
+          filtered.map(data => <li key={data.id}>{data.nome}</li>)
       }
     </main>
-  );
-};
+  )
+}
 
-export default ListaFiltrada;
+export default App
