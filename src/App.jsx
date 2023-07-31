6cd515d8d2376045f2b4f6ad8a4edc8573021b97
Guilherme Henrique <44971353+MrGuizao@users.noreply.github.com>
2023-07-31 15:58:50 -0300

    31/07 USEREDUCER

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,12 @@
-import React from 'react';
-import { useState } from 'react';
+import React from 'react'
+import DateCounter from './components/Reducer/DateCounter';
 
 const App = () => {
-  const [todos, setTodos] = useState([]);
-  const [task, setTask] = useState({ name: '', age: '', size: '' });
-  const [click, setClick] = useState(null);
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    setTodos([...todos, task]);
-    setTask({ name: '', age: '', size: '' });
-  }
-  const handleChange = e => setTask({ ...task, id: crypto.randomUUID(), [e.target.name]: e.target.value })
-  const handleClick = id => setClick(id);
-  const handleDelete = id => setTodos(todos.filter(todo => todo.id !== id));
-
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={task.name} name='name' placeholder='name' onChange={handleChange} />
-        <input type="text" value={task.age} name='age' placeholder='age' onChange={handleChange} />
-        <input type="text" value={task.size} name='size' placeholder='size' onChange={handleChange} />
-        <button>add</button>
-      </form>
-      <ul>
-        {
-          todos.map(todo => <li
-            key={todo.id}
-            onClick={() => handleClick(todo.id)}
-            style={click === todo.id ? { textDecoration: 'line-through' } : null}>
-            {todo.name} tem {todo.age} anos de idade e altura de {todo.size} Cm
-            <span onClick={() => handleDelete(todo.id)}>❌</span>
-          </li>)
-        }
-      </ul>
-    </>
+    <div>
+      <DateCounter />
+    </div>
   )
 }
 
-export default App;
+export default App
