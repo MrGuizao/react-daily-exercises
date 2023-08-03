bafdd18748b674dcd99907f8860597320dc6ef5c
Guilherme Henrique <44971353+MrGuizao@users.noreply.github.com>
2023-08-03 09:38:09 -0300

    03/08 FILTRO PESQUISA

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
-import DateCounter from './components/Reducer/DateCounter';
+import { useState } from 'react'
 
 const App = () => {
+  const [datas, setDatas] = useState([
+    { id: 0, nome: 'guilherme' },
+    { id: 1, nome: 'giovanna' },
+    { id: 2, nome: 'barbara' },
+    { id: 3, nome: 'milena' },
+  ])
+  const [search, setSearch] = useState('');
+  const filteredArray = datas.filter(data => data.nome.toLowerCase().includes(search.toLowerCase()))
+
+
   return (
-    <div>
-      <DateCounter />
-    </div>
+    <>
+      <main>
+        <input type="text" value={search} onChange={(e) => { setSearch(e.target.value) }} />
+      </main>
+      <ul>
+        {
+          filteredArray ?
+            filteredArray.map(data => <li key={data.id}>{data.nome}</li>)
+            :
+            <h1>sem resultado</h1>
+        }
+      </ul>
+    </>
   )
 }
 
