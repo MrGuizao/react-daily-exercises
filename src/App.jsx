96f40ca8408a26c0f4c11b4cf0cc8b12cf539665
Guilherme Henrique <44971353+MrGuizao@users.noreply.github.com>
2023-08-04 11:03:56 -0300

    04/08 USE REDUCER EXEMPLOS

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,78 @@
-import React, { useEffect, useReducer } from 'react'
-import Header from './Reducer/Header';
-import Main from './Reducer/Main';
-import Loader from './Reducer/Loader';
-import StartScreen from './Reducer/StartScreen';
-import Error from './Reducer/Error';
-import Question from './Reducer/Question';
-
+import React from 'react'
+import { useReducer } from 'react'
 
 const initialState = {
-  questions: [],
-  status: 'loading',
-  index: 0,
-  answer: null,
-  points: 0,
+  count: 0,
+  text: '',
+  one: 0,
+  two: 0,
+  random: null,
 }
 
-function reducer(state, action) {
+const reducer = (state, action) => {
   switch (action.type) {
-    case 'dataReceived':
-      return { ...state, questions: action.payload, status: 'ready' }
-    case 'start':
-      return { ...state, status: 'active' }
-    case 'newAnswer':
-      const question = state.questions.at(state.index);
-      return { ...state, answer: action.payload, points: action.payload === question.correctOption ? state.points + 1 : state.points }
-    case 'dataFailed':
-      return { ...state, status: 'error' }
-    default: throw new Error('Action unknown')
+    case 'add': return { ...state, count: state.count + 1 }
+    case 'sub': return { ...state, count: state.count - 1 }
+    case 'write': return { ...state, text: action.payload }
+    case 'soma': return { ...state, one: state.one + 5, two: state.two + 10, }
+    case 'random': return { ...state, random: Math.floor(Math.random() * 25) + 1, }
+    default: throw new Error('cu');
+
   }
 }
 
 const App = () => {
-  const [{ questions, status, index, answer, points }, dispatch] = useReducer(reducer, initialState);
-  const numQuestion = questions.length;
+  const [{ count, text, one, two, random }, dispatch] = useReducer(reducer, initialState);
+  const three = one + two;
 
-  useEffect(() => {
-    fetch('http://localhost:8000/questions')
-      .then(response => response.json())
-      .then(data => dispatch({ type: 'dataReceived', payload: data }))
-      .catch(dispatch({ type: 'dataFailed' }))
-  }, [])
 
   return (
-    <div className='app'>
-      <Header />
-      <Main>
-        {status === 'loading' && <Loader />}
-        {status === 'ready' && <StartScreen numQuestion={numQuestion} dispatch={dispatch} />}
-        {status === 'active' && <Question question={questions[index]} dispatch={dispatch} answer={answer} />}
-        {status === 'error' && <Error />}
-      </Main>
+    <div>
+      <div style={{ backgroundColor: 'green', padding: '20px' }}>
+        <h4>testando soma</h4>
+        <h1>{count}</h1>
+        <button className='btn' onClick={() => { dispatch({ type: 'add' }) }}>Add</button>
+        <button className='btn' onClick={() => { dispatch({ type: 'sub' }) }}>Sub</button>
+      </div>
+
+      <br />
+      <br />
+
+      <div style={{ backgroundColor: 'red', padding: '20px' }}>
+        <h4>testando inputs value</h4>
+        <h1>{text}</h1>
+        <input type="text" value={text} onChange={(e) => { dispatch({ type: 'write', payload: e.target.value }) }} />
+      </div>
+
+      <br />
+      <br />
+
+      <div style={{ backgroundColor: 'purple', padding: '20px' }}>
+        <h4>testando derived state</h4>
+        <h2>one: {one}</h2>
+        <h2>two: {two}</h2>
+        <h2>total: {three}</h2>
+        <button className="btn" onClick={() => { dispatch({ type: 'soma' }) }}>plus</button>
+      </div>
+
+      <br />
+      <br />
+
+      <div style={{ backgroundColor: 'chocolate', padding: '20px' }}>
+        <h4>testando numero aleatorio</h4>
+        <h2>numero aleatório: {random}</h2>
+        <button className="btn" onClick={() => { dispatch({ type: 'random' }) }}>roll</button>
+      </div>
+
+      <br />
+      <br />
+
+      <div style={{ backgroundColor: 'gray', padding: '20px' }}>
+        <h4>testando multiplos inputs</h4>
+        <button className="btn" onClick={() => { dispatch({ type: 'random' }) }}>roll</button>
+      </div>
     </div>
   )
 }
 
-export default App;
+export default App
