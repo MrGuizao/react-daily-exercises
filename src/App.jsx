f0c2f3a73cc39c724031d491812ce39beef7d98f
Guilherme Henrique <44971353+MrGuizao@users.noreply.github.com>
2023-08-04 09:34:35 -0300

    04/08 USEREDUCER QUIZ

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,56 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useEffect, useReducer } from 'react'
+import Header from './Reducer/Header';
+import Main from './Reducer/Main';
+import Loader from './Reducer/Loader';
+import StartScreen from './Reducer/StartScreen';
+import Error from './Reducer/Error';
+import Question from './Reducer/Question';
+
+
+const initialState = {
+  questions: [],
+  status: 'loading',
+  index: 0,
+  answer: null,
+  points: 0,
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...state, questions: action.payload, status: 'ready' }
+    case 'start':
+      return { ...state, status: 'active' }
+    case 'newAnswer':
+      const question = state.questions.at(state.index);
+      return { ...state, answer: action.payload, points: action.payload === question.correctOption ? state.points + 1 : state.points }
+    case 'dataFailed':
+      return { ...state, status: 'error' }
+    default: throw new Error('Action unknown')
+  }
+}
 
 const App = () => {
-  const [todos, setTodos] = useState([
-    { id: 0, nome: 'guilherme', isComplete: false },
-    { id: 1, nome: 'giovanna', isComplete: false },
-    { id: 2, nome: 'barbara', isComplete: false },
-    { id: 3, nome: 'milena', isComplete: false },
-  ])
-  const [datas, setDatas] = useState({ nome: '', idade: '', isComplete: false });
-  const [clicked, setClicked] = useState(null);
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(reducer, initialState);
+  const numQuestion = questions.length;
 
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    setTodos([...todos, datas]);
-    setDatas({ nome: '', idade: '', isComplete: false });
-  }
-  function handleChange(e) {
-    setDatas({ ...datas, id: crypto.randomUUID(), [e.target.name]: e.target.value });
-  }
-  function handleDelete(id) {
-    setTodos(todos.filter(todo => todo.id !== id))
-  }
-  function handleUpdate(id) {
-    setTodos(todos.map(todo => id === todo.id ? { ...todo, isComplete: !todo.isComplete } : todo))
-  }
+  useEffect(() => {
+    fetch('http://localhost:8000/questions')
+      .then(response => response.json())
+      .then(data => dispatch({ type: 'dataReceived', payload: data }))
+      .catch(dispatch({ type: 'dataFailed' }))
+  }, [])
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={datas.nome} onChange={handleChange} name='nome' placeholder='nome...' />
-        <input type="text" value={datas.idade} onChange={handleChange} name='idade' placeholder='idade...' />
-        <button>add</button>
-      </form>
-      <ul>
-        {
-          todos.map(todo => <div
-            style={todo.isComplete ? { textDecoration: 'line-through' } : null}
-            onClick={() => setClicked(todo.id)}>
-            <li key={todo.id}>{todo.nome} tem {todo.idade} anos</li>
-            <span onClick={() => handleUpdate(todo.id)}>✔</span>
-            <span onClick={() => handleDelete(todo.id)}>❌</span>
-          </div>)
-        }
-      </ul>
-    </>
+    <div className='app'>
+      <Header />
+      <Main>
+        {status === 'loading' && <Loader />}
+        {status === 'ready' && <StartScreen numQuestion={numQuestion} dispatch={dispatch} />}
+        {status === 'active' && <Question question={questions[index]} dispatch={dispatch} answer={answer} />}
+        {status === 'error' && <Error />}
+      </Main>
+    </div>
   )
 }
 
