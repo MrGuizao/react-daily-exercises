38ab48aeb74451293191177114f839bab98948ab
Guilherme Henrique <44971353+MrGuizao@users.noreply.github.com>
2023-08-07 09:31:32 -0300

    07/08 TODO COM REDUCER

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,89 @@
-import React from 'react'
-import { useReducer } from 'react'
+import React, { useReducer, useState } from 'react';
 
+// Definindo o estado inicial do reducer
 const initialState = {
-  count: 0,
-  text: '',
-  one: 0,
-  two: 0,
-  random: null,
-}
+  todos: [],
+};
 
-const reducer = (state, action) => {
+// Definindo as ações para atualizar o estado
+const actionTypes = {
+  ADD_TODO: 'ADD_TODO',
+  TOGGLE_TODO: 'TOGGLE_TODO',
+  DELETE_TODO: 'DELETE_TODO',
+};
+
+// Reducer que trata as ações para atualizar o estado
+const todoReducer = (state, action) => {
   switch (action.type) {
-    case 'add': return { ...state, count: state.count + 1 }
-    case 'sub': return { ...state, count: state.count - 1 }
-    case 'write': return { ...state, text: action.payload }
-    case 'soma': return { ...state, one: state.one + 5, two: state.two + 10, }
-    case 'random': return { ...state, random: Math.floor(Math.random() * 25) + 1, }
-    default: throw new Error('cu');
-
+    case actionTypes.ADD_TODO:
+      return { ...state, todos: [...state.todos, action.payload] };
+    case actionTypes.TOGGLE_TODO:
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload ? { ...todo, completed: !todo.completed } : todo
+        ),
+      };
+    case actionTypes.DELETE_TODO:
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.payload),
+      };
+    default:
+      return state;
   }
-}
+};
 
-const App = () => {
-  const [{ count, text, one, two, random }, dispatch] = useReducer(reducer, initialState);
-  const three = one + two;
+const TodoList = () => {
+  const [state, dispatch] = useReducer(todoReducer, initialState);
+  const [newTodo, setNewTodo] = useState('');
 
+  const handleInputChange = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() !== '') {
+      const newTodoItem = {
+        id: Date.now(),
+        text: newTodo,
+        completed: false,
+      };
+      dispatch({ type: actionTypes.ADD_TODO, payload: newTodoItem });
+      setNewTodo('');
+    }
+  };
+
+  const handleToggleTodo = (id) => {
+    dispatch({ type: actionTypes.TOGGLE_TODO, payload: id });
+  };
+
+  const handleDeleteTodo = (id) => {
+    dispatch({ type: actionTypes.DELETE_TODO, payload: id });
+  };
 
   return (
     <div>
-      <div style={{ backgroundColor: 'green', padding: '20px' }}>
-        <h4>testando soma</h4>
-        <h1>{count}</h1>
-        <button className='btn' onClick={() => { dispatch({ type: 'add' }) }}>Add</button>
-        <button className='btn' onClick={() => { dispatch({ type: 'sub' }) }}>Sub</button>
+      <h1>Todo List</h1>
+      <div>
+        <input type="text" value={newTodo} onChange={handleInputChange} />
+        <button onClick={handleAddTodo}>Add Todo</button>
       </div>
-
-      <br />
-      <br />
-
-      <div style={{ backgroundColor: 'red', padding: '20px' }}>
-        <h4>testando inputs value</h4>
-        <h1>{text}</h1>
-        <input type="text" value={text} onChange={(e) => { dispatch({ type: 'write', payload: e.target.value }) }} />
-      </div>
-
-      <br />
-      <br />
-
-      <div style={{ backgroundColor: 'purple', padding: '20px' }}>
-        <h4>testando derived state</h4>
-        <h2>one: {one}</h2>
-        <h2>two: {two}</h2>
-        <h2>total: {three}</h2>
-        <button className="btn" onClick={() => { dispatch({ type: 'soma' }) }}>plus</button>
-      </div>
-
-      <br />
-      <br />
-
-      <div style={{ backgroundColor: 'chocolate', padding: '20px' }}>
-        <h4>testando numero aleatorio</h4>
-        <h2>numero aleatório: {random}</h2>
-        <button className="btn" onClick={() => { dispatch({ type: 'random' }) }}>roll</button>
-      </div>
-
-      <br />
-      <br />
-
-      <div style={{ backgroundColor: 'gray', padding: '20px' }}>
-        <h4>testando multiplos inputs</h4>
-        <button className="btn" onClick={() => { dispatch({ type: 'random' }) }}>roll</button>
-      </div>
+      <ul>
+        {state.todos.map((todo) => (
+          <li key={todo.id}>
+            <span
+              style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+              onClick={() => handleToggleTodo(todo.id)}
+            >
+              {todo.text}
+            </span>
+            <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default TodoList;
