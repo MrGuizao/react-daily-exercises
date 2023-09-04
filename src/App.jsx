a303f5c4d150e7fb5803d0d66bff2f2397f3566d
Guilherme Henrique <44971353+MrGuizao@users.noreply.github.com>
2023-09-04 10:28:05 -0300

    MULTIPLES INPUTS REVIEW

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,97 @@
-import React, { useRef } from 'react';
-import { useFrame } from '@react-three/fiber';
-import { useGLTF } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-
-
-const Model = () => {
-  const gltf = useGLTF('/models/fones.glb');
-  const modelRef = useRef();
-  useFrame(() => modelRef.current.rotation.y += 0.005);
-  return <primitive object={gltf.scene} ref={modelRef} scale={[1, 1, 1]} />; // Ajuste a escala aqui
-};
-
+import React from 'react'
+import { useState } from 'react'
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState({ name: '', age: '' });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos, task]);
+    setTask({ name: '', age: '' });
+  }
+
+  function handleChange(e) {
+    setTask({ ...task, id: crypto.randomUUID(), [e.target.name]: e.target.value, });
+  }
+
+  function handleDelete(id) {
+    setTodos(todos.filter(todo => todo.id !== id));
+  }
+
   return (
-    <div className="App">
-      <Canvas>
-        <ambientLight />
-        <directionalLight position={[0, 50, 0]} intensity={4} />
-        <OrbitControls />
-        <Model />
-      </Canvas>
-    </div>
-  );
-};
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={task.name} name='name' onChange={handleChange} placeholder='nome...' />
+        <input type="text" value={task.age} name='age' onChange={handleChange} placeholder='age...' />
+        <button>add</button>
+      </form>
+      <ul>
+        {
+          todos.map(todo => <li key={todo.id}>{todo.name} tem {todo.age} anos<button onClick={() => handleDelete(todo.id)}>delete</button></li>)
+        }
+      </ul>
+    </>
+  )
+}
 
 export default App;
+
+
+
+
+
+// import React, { useState } from 'react';
+
+// const App = () => {
+//   const [todos, setTodos] = useState([]);
+//   const [task, setTask] = useState({ name: '', age: '' });
+
+//   function handleSubmit(e) {
+//     e.preventDefault();
+//     setTodos([...todos, task]);
+//     setTask({ name: '', age: '' });
+//   }
+
+//   function handleChange(e) {
+//     setTask({ ...task, id: crypto.randomUUID(), [e.target.name]: e.target.value });
+//   }
+
+//   function handleDelete(id) {
+//     setTodos(todos.filter(todo => todo.id !== id));
+//   }
+
+//   return (
+//     <>
+//       <form onSubmit={handleSubmit}>
+//         <input
+//           type="text"
+//           value={task.name}
+//           name='name'
+//           onChange={handleChange}
+//           placeholder='Nome...'
+//           required // Defina o campo como obrigatório
+//         />
+//         <input
+//           type="text"
+//           value={task.age}
+//           name='age'
+//           onChange={handleChange}
+//           placeholder='Idade...'
+//           required // Defina o campo como obrigatório
+//         />
+//         <button type="submit">Adicionar</button>
+//       </form>
+//       <ul>
+//         {todos.map((todo) => (
+//           <li key={todo.id}>
+//             {todo.name} tem {todo.age} anos
+//             <button onClick={() => handleDelete(todo.id)}>Deletar</button>
+//           </li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// }
+
+// export default App;
